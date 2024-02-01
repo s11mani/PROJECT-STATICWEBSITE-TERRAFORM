a3ccca2e0129bfa1932994f2cbf30e4ccbe26867
root <root@ip-172-31-42-129.ap-south-1.compute.internal>
2024-02-01 18:43:56 +0000

    update enable acl and public access

--- main.tf
+++ main.tf
@@ -1,7 +1,36 @@
+## to create s3 bucket
 resource "aws_s3_bucket" "example" {
   bucket = var.aws_bucket_name
   tags = {
     Name        = "My-bucket"
     Environment = "Dev-test"
   }
+}
+
+## to enable bucket owner-ship
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+## to enable public acess
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket                  = aws_s3_bucket.example.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+## to enable acl
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "public-read"
 }
